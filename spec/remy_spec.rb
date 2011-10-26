0f06f78761dc690c172edf44b7ccb2a72def900f
Greg Woodward <gregwoodward0@gmail.com>
2011-10-25 18:09:10 -0700

    Moved the LittleChef specs out into their own file.

--- spec/remy_spec.rb
+++ spec/remy_spec.rb
@@ -84,31 +84,5 @@ describe Remy do
       JSON.parse(subject.to_json).should == {"cookbook_path"=>["bar"], "remote_chef_dir"=>"foo", "roles_path"=>["blech"], "baz"=>"baz", "yml_files"=>["fixtures/foo.yml", "fixtures/bar.yml"], "blah"=>"bar"}
     end
   end
-
-  describe '.run_chef_remote' do
-    it 'should work with a hash' do
-      #Remy.expects(:execute).with("ssh root@111.111.111.111 'chef_solo'")
-      little_chef = Remy::LittleChef.new(:public_ip => '50.57.150.171', :run_list => ['recipe[hello_world::default]'])
-      little_chef.run
-      little_chef.configuration.public_ip.should == '50.57.150.171'
-      little_chef.configuration.run_list.should == ['recipe[hello_world::default]']
-    end
-
-    it 'should work with JSON' do
-      #Remy.expects(:execute).with("ssh root@111.111.111.111 'chef_solo'")
-      little_chef = Remy::LittleChef.new("{\"public_ip\":\"50.57.150.171\",\"run_list\":[\"recipe[hello_world::default]\"]}")
-      little_chef.run
-      little_chef.configuration.public_ip.should == '50.57.150.171'
-      little_chef.configuration.run_list.should == ['recipe[hello_world::default]']
-    end
-
-    it 'should not modify the global Remy config, but rather only the config for this Chef run which is used to generate the JSON' do
-      Remy.configuration.another_node_attribute.should == 'red'
-      little_chef = Remy::LittleChef.new(:another_node_attribute.should == 'blue')
-      little_chef.configuration.another_node_attribute.should == 'blue'
-      little_chef.configuration.yml_files.should == ['fixtures/foo.yml', 'fixtures/bar.yml']
-      Remy.configuration.another_node_attribute.should == 'red'
-    end
-  end
 end
 
