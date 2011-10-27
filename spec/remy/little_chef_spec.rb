f0a5d06a4fcee9acb7a9450fc17d67b56d705042
Greg Woodward <gregwoodward0@gmail.com>
2011-10-26 18:19:35 -0700

    Refactoring the little_chef_spec.rb.

--- spec/remy/little_chef_spec.rb
+++ spec/remy/little_chef_spec.rb
@@ -21,7 +21,7 @@ describe Remy::LittleChef do
   end
 
   describe '#run' do
-    it 'should work with a hash' do
+    it 'should work with a hash as its argument' do
       #Remy.expects(:execute).with("ssh root@111.111.111.111 'chef_solo'")
       little_chef = Remy::LittleChef.new(:chef_ip => '50.57.162.227') 
       little_chef.run
@@ -29,7 +29,7 @@ describe Remy::LittleChef do
       little_chef.configuration.recipes.should == ['recipe[hello_world]']
     end
 
-    it 'should work with JSON' do
+    it 'should work with JSON as its argument' do
       #Remy.expects(:execute).with("ssh root@111.111.111.111 'chef_solo'")
       little_chef = Remy::LittleChef.new("{\"chef_ip\":\"50.57.162.227\"}")
       little_chef.run
@@ -37,12 +37,14 @@ describe Remy::LittleChef do
       little_chef.configuration.recipes.should == ['recipe[hello_world]']
     end
 
-    it 'should not modify the global Remy config, but rather only the config for this Chef run which is used to generate the JSON' do
-      Remy.configuration.another_node_attribute.should == 'red'
-      little_chef = Remy::LittleChef.new(:another_node_attribute.should == 'blue')
-      little_chef.configuration.another_node_attribute.should == 'blue'
-      little_chef.configuration.yml_files.should == ['fixtures/foo.yml', 'fixtures/bar.yml']
-      Remy.configuration.another_node_attribute.should == 'red'
+    it 'should not modify the global Remy config, but rather only the config which is for this particular Chef node' do
+      original_global_remy_node_attribute_value = Remy.configuration.another_node_attribute
+      original_global_remy_node_attribute_value.should == 'red'
+      new_attribute_value_for_this_node_only = 'blue'
+      little_chef = Remy::LittleChef.new(:chef_ip => '50.57.162.227', :another_node_attribute => new_attribute_value_for_this_node_only)
+      little_chef.configuration.another_node_attribute.should == new_attribute_value_for_this_node_only
+      little_chef.configuration.yml_files.should == ['../fixtures/foo.yml', '../fixtures/bar.yml', '../fixtures/little_chef.yml']
+      Remy.configuration.another_node_attribute.should == original_global_remy_node_attribute_value  # Unchanged
     end
   end
 end
